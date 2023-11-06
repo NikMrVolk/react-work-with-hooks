288be279045b4b8f16896ad8dd6bbe2504485fd4
NickZavadski <nick.mr.volk@gmail.com>
2023-11-06 23:02:24 +0300

    add useInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useInput } from './hooks/useInput'
+
 function App() {
-	return <div></div>
+	const username = useInput('')
+	const password = useInput('')
+
+	return (
+		<div>
+			<input {...username} type="text" />
+			<input {...password} type="password" />
+			<button
+				onClick={() => {
+					console.log(username.value, password.value)
+				}}
+			>
+				push
+			</button>
+		</div>
+	)
 }
 
 export default App
